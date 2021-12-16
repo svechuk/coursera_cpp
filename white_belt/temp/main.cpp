0fd6719a642a979f4ea2ad248a47cc9301b09da9
Zheleznova Svetlana <svechukhaha@gmail.com>
2021-12-16 16:47:26 +0300

    task 3 optional

--- white_belt/temp/main.cpp
+++ white_belt/temp/main.cpp
@@ -2,19 +2,13 @@
 using namespace std;
 
 int main() {
-	double a, b, x, y, n, final;
-	cin >> n >> a >> b >> x >> y;
+	int a, b;
+	cin >> a >> b;
 
-	if (n > b) {
-		final = n * (1 - y / 100);
+	for (int i = a; i <= b; ++i) {
+		if (i % 2 == 0) {
+			cout << i << " ";
+		}
 	}
-	else if (n > a) {
-		final = n * (1 - x / 100);
-	}
-	else {
-		final = n;
-	}
-
-	cout << final << endl;
 	return 0;
 }
